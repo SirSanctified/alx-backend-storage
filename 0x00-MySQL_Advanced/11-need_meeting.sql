3f8902efc5ff92320b1e5746ca287eecffeca5ad
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-04-07 01:07:24 +0200

    Refactor code

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,4 +2,4 @@
 
 CREATE VIEW need_meeting AS
 SELECT * FROM users
-WHERE average_score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
+WHERE average_score < 80 AND (last_meeting IS NULL OR last_meeting < DATEADD(month, -1, GETDATE()));
