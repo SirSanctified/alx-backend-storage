6de05d6048e80322b24aaa68a3df1910b383112d
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-04-07 12:08:00 +0200

    Add delimiter

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,11 @@
 -- create a trigger that decreases the quantity of an item after adding a new order.
 
+DELIMITER //
 CREATE OR REPLACE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW 
 BEGIN
     UPDATE items
     SET quantity = quantity - 1 WHERE name = NEW.item_name;
-END;
+END//
+DELIMITER ;
