132606025a07965aca0fe4cb16611613e344ee26
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-04-07 12:31:41 +0200

    Refector code for readability

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,15 @@
 -- create a trigger that decreases the quantity of an item after adding a new order.
 
 DELIMITER //
+
 CREATE OR REPLACE TRIGGER decrease_item_quantity
-AFTER INSERT ON orders
+AFTER INSERT ON orders 
 FOR EACH ROW 
 BEGIN
     UPDATE items
-    SET quantity = quantity - 1 WHERE name = NEW.item_name;
-END//
+    SET quantity = quantity - 1 
+    WHERE name = NEW.item_name;
+END //
+
 DELIMITER ;
+
