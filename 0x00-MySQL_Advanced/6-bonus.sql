6037ed7cfb29ea5bc6291d09b9e499bd262fdf4b
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-04-07 12:55:31 +0200

    Remove the replace in create a procedure

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,7 +1,7 @@
 -- create a stored procedure AddBonus that adds a new correction for a student.
 
 DELIMITER //
-CREATE OR REPLACE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
     DECLARE project INT;
     SELECT id INTO project FROM projects WHERE name = project_name;
