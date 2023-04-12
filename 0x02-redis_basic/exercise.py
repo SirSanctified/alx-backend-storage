002c14896857dc5196b7980933785ebb6a267221
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-04-12 12:49:58 +0200

    Create a decorator method to count Cache's method calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,20 @@ Writing strings to Redis
 import redis
 from uuid import uuid4
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Counts how many times methods of the Cache class are called."""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Wrapper function"""
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+
+    return wrapper
 
 
 class Cache:
